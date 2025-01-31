8095f4312e34c3bd5a4add8e4d02f5a3a82ea16d
jcdc-chewy <jcdc_chewy@proton.me>
2025-01-31 17:56:19 +0800

    [GCD_METHOD] Constructed the gcd() method

--- Fraction.py
+++ Fraction.py
@@ -4,9 +4,24 @@ class Fraction(object):
         #TODO
         pass
 
-    def gcd(a, b):
-        #TODO
-        pass
+    def gcd(firstNumber, secondNumber):
+        '''
+        Gets the Greatest Common Denominator of firstNumber and secondNumber
+        '''
+
+        firstNumber = abs(firstNumber)
+        secondNumber = abs(secondNumber)
+
+        if firstNumber == 0 or secondNumber == 0:
+            return 0
+
+        elif firstNumber == secondNumber:
+            return firstNumber
+
+        else: 
+            for i in range(min(firstNumber, secondNumber), 0, -1):
+                if firstNumber % i == 0 and secondNumber % i == 0:
+                    return i
 
     def get_numerator(self):
         #TODO
